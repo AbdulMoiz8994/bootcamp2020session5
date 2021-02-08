73a20f0d6eb0b94d860146f10bad05b6a0e0ae52
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-08 10:14:43 +0500

    we are doing final commit

--- src/ChildContext.jsx
+++ src/ChildContext.jsx
@@ -5,7 +5,7 @@ export const ChildContext = () => {
     console.log(UseContext)
     return (
         <div>
-            <h1>This is the value of {UseContext[0]}</h1>
+            <h1>This is the value of= {UseContext[0]}</h1>
             <button onClick={() => UseContext[1](++UseContext[0])}>Update Number</button>
             {/* <h2>Hell world here is {props.name}</h2> */}
         </div>
